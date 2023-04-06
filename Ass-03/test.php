ac7f3111c35db8e70cc75e60d72e56a15c404528
code-arif <arifulislam6460@gmail.com>
2023-04-06 22:25:51 +0600

    modified file

--- Ass-03/test.php
+++ Ass-03/test.php
@@ -1,15 +1,1 @@
-<center id="test" >
-    <?php
-    function pyramid($hm)
-    {
-        for ($i = 1; $i <= $hm; $i++) {
-
-            for ($j = 1; $j <= $i; $j++) {
-                echo '*';
-            }
-            echo '<br/>';
-        }
-    }
-    pyramid(1000);
-    ?>
-</center>
+//////this is demo file.
